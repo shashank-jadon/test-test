6d3998ee12de57f2118995e738d19653131b21ac
shashank-jadon <109354393+shashank-jadon@users.noreply.github.com>
2022-08-16 18:41:34 +0530

    cpanel-install
    Testing

---
+++ install.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+yum update -y
+yum install wget perl screen -y
+VPSUSER={{username}}
+echo $VPSUSER
+HOSTNAME={{vps_hostname}}
+IFS="." read -a HOSTNAME_ARR <<< $HOSTNAME
+USERNAME=${HOSTNAME_ARR[1]}
+CONTACTEMAIL=contact@floohost.ml
+DOMAIN="${HOSTNAME_ARR[1]}.${HOSTNAME_ARR[2]}"
+systemctl get-default
+whoami
+su root
+sleep 5
+cd /home
+wget https://securedownloads.cpanel.net/latest
+screen -S cpanelinstall -L -d -m sh latest 
+sleep 5
+while screen -list | grep cpanelinstall
+do
+    sleep 10
+done
+echo "Script Finished"
